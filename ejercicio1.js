b4559f3b5557d42b84182941a15592dff3a512be
wolf813 <danielemorales7@gmail.com>
2022-10-24 10:50:51 -0600

    Tarea 2

---
+++ ejercicio1.js
@@ -0,0 +1,27 @@
+// Programa en Node.js demostracion de metodo fs.readFileSync()
+
+// Require del modulo fs de Node.js
+const fs = require('fs');
+
+// Uso del metodo fs.readFile()
+// lectura del archivo 'data.json'
+console.log('Iniciamos la aplicacion')
+fs.readFile('./data.json',
+		{encoding:'utf8', flag:'r'},
+		function(err, data) {
+	if(err)
+		console.log(err);
+	else
+		console.log('evento 1',JSON.parse(data));
+});
+console.log('Finaliza la primer lectura del archivo data.json')
+
+// Usamos el metodo fs.readFileSync()
+// lectura del archivo 'data.json'
+console.log('iniciamos la lectura del segundo archivo')
+const data = fs.readFileSync('./data.json',
+			{encoding:'utf8', flag:'r'});
+
+// mostramos la informacion
+console.log('evento 2',JSON.parse(data));
+console.log('fin del programa')
