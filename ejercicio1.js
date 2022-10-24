80951186145d3cca1cdd50b8299700bb04fb3f80
wolf813 <danielemorales7@gmail.com>
2022-10-24 16:33:58 -0600

    Upt v2

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,10 +1,6 @@
-// Programa en Node.js demostracion de metodo fs.readFileSync()
 
-// Require del modulo fs de Node.js
 const fs = require('fs');
 
-// Uso del metodo fs.readFile()
-// lectura del archivo 'data.json'
 console.log('Iniciamos la aplicacion')
 fs.readFile('./DB/data.json',
 		{encoding:'utf8', flag:'r'},
@@ -16,12 +12,9 @@ fs.readFile('./DB/data.json',
 });
 console.log('Finaliza la primer lectura del archivo data.json')
 
-// Usamos el metodo fs.readFileSync()
-// lectura del archivo 'data.json'
 console.log('iniciamos la lectura del segundo archivo')
 const data = fs.readFileSync('./DB/data.json',
 			{encoding:'utf8', flag:'r'});
 
-// mostramos la informacion
 console.log('evento 2',JSON.parse(data));
 console.log('fin del programa')
